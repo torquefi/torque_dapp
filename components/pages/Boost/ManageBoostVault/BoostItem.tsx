abcf5a21b386c8ad2dea84ef265f38f04d30e81a
NguyenThuan <thuans2nd@gmail.com>
2023-05-09 19:15:02 +0700

    Update

--- components/pages/Boost/ManageBoostVault/BoostItem.tsx
+++ components/pages/Boost/ManageBoostVault/BoostItem.tsx
@@ -65,7 +65,7 @@ export function BoostItem({ item }: any) {
           tokenSymbol={item?.token}
           tokenValue={item.deposited}
           usdDefault
-          className="-my-4 flex h-full min-w-[100px] flex-col items-center justify-center gap-2 py-4"
+          className="-my-4 flex h-full min-w-[130px] flex-col items-center justify-center gap-2 py-4"
           render={(value) => (
             <>
               <p className="text-[22px]">{value}</p>
@@ -80,7 +80,7 @@ export function BoostItem({ item }: any) {
           tokenSymbol={item?.token}
           tokenValue={item.earnings}
           usdDefault
-          className="-my-4 flex h-full min-w-[100px] flex-col items-center justify-center gap-2 py-4"
+          className="-my-4 flex h-full min-w-[130px] flex-col items-center justify-center gap-2 py-4"
           decimalScale={2}
           render={(value) => (
             <>
@@ -91,7 +91,7 @@ export function BoostItem({ item }: any) {
             </>
           )}
         />
-        <div className="flex min-w-[100px] flex-col items-center justify-center gap-2">
+        <div className="flex min-w-[130px] flex-col items-center justify-center gap-2">
           <div className="text-[22px]">{item.APR}</div>
           <div className="font-mona text-[14px] text-[#959595]">
             Variable APR
