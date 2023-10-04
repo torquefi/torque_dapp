681fff09f6d700505554dafc51b1877e90fde142
ravenvn <nanyangbk@gmail.com>
2023-10-04 22:44:40 +0700

    fix build

--- types/store.ts
+++ types/store.ts
@@ -1,11 +1,11 @@
 import { IAuth } from '@/lib/redux/auth/auth'
-import { IDataUser } from '@/lib/redux/auth/dataUser'
+import { IBorrowStore } from '@/lib/redux/slices/borrow'
 import { IthemeType } from '@/lib/redux/slices/theme'
 import { IUsdPrice } from '@/lib/redux/slices/usdPrice'
 
 export interface AppStore {
   usdPrice?: IUsdPrice
   auth?: IAuth
-  dataUser?: IDataUser
+  borrow?: IBorrowStore
   theme?: IthemeType
 }
