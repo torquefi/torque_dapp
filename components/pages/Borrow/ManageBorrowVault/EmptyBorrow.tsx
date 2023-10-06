a805a8d5f49ee46506e0bb8a5088c80cd2ac5185
wormbsc <camconrad@gmail.com>
2023-10-06 12:04:13 -0700

    refine footer & empty states

--- components/pages/Borrow/ManageBorrowVault/EmptyBorrow.tsx
+++ components/pages/Borrow/ManageBorrowVault/EmptyBorrow.tsx
@@ -25,10 +25,10 @@ export const EmptyBorrow = () => {
           }
           alt=""
         />
-        <p className="font-larken text-[28px] text-black dark:text-white">
+        <p className="font-larken text-[24px] text-black dark:text-white">
           No vaults created yet
         </p>
-        <p className="mt-[16px] max-w-[400px] text-center font-body text-[18px] text-[#959595]">
+        <p className="mt-[12px] max-w-[360px] text-center font-body text-[16px] text-[#959595]">
           Deposit assets to create a Borrow vault & access a self-service line
           of credit. Repay as you prefer.
         </p>
