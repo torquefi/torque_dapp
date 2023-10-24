40f7e66a5d3b55e0a68bb720723df72634db86b2
wormbsc <camconrad@gmail.com>
2023-10-24 10:24:22 -0700

    add min duration & early exit fee /boost

--- components/pages/Boost/CreateBoostVault/createBoostItem.tsx
+++ components/pages/Boost/CreateBoostVault/createBoostItem.tsx
@@ -237,7 +237,15 @@ export function CreateBoostItem({ item }: any) {
         </div>
         <div className="font-mona flex w-full items-center justify-between text-[16px] text-[#959595]">
           <div className="font-mona">Variable APY</div>
-          <div className="">0%</div>
+          <div className="">0.00%</div>
+        </div>
+        <div className="font-mona flex w-full items-center justify-between py-[14px] text-[16px] text-[#959595]">
+          <div className="font-mona">Min. duration</div>
+          <div className="">7 days</div>
+        </div>
+        <div className="font-mona flex w-full items-center justify-between text-[16px] text-[#959595]">
+          <div className="font-mona">Early exit fee</div>
+          <div className="">0.00%</div>
         </div>
         <button
           className={`font-mona mt-4 w-full rounded-full border border-[#AA5BFF] bg-gradient-to-b from-[#AA5BFF] to-[#912BFF] py-1 uppercase text-white transition-all hover:border hover:border-[#AA5BFF] hover:from-transparent hover:to-transparent hover:text-[#AA5BFF]
