0f2403cf2d171f5d7a809ada07d54e9033047c35
wormbsc <camconrad@gmail.com>
2023-12-21 06:44:58 -0800

    refine

--- components/pages/Vote/Distribution/InforTORQ.tsx
+++ components/pages/Vote/Distribution/InforTORQ.tsx
@@ -16,7 +16,7 @@ export const InforTORQ = () => {
         </p>
       </div>
       <div className="w-full md:w-[32%] rounded-[12px] border border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-[#ffffff] dark:bg-transparent dark:bg-gradient-to-br from-[#0d0d0d] to-[#0d0d0d]/0 text-[#404040] dark:text-white text-center px-[36px] py-[20px]">
-        <h4 className="text-[24px] font-larken font-[400] leading-[40px]">16B</h4>
+        <h4 className="text-[24px] font-larken font-[400] leading-[40px]">16,000,000,000</h4>
         <p className="font-[500] leading-[24px] text-[#959595]">
           TORQ Remaining
         </p>
--- components/pages/Vote/index.tsx
+++ components/pages/Vote/index.tsx
@@ -1,16 +1,10 @@
 import React from 'react'
 import { Governance } from './Governance'
-import { Distribution } from './Distribution'
-import { Leaderboard } from './Leaderboard'
-import { DetailsVotes } from './Details'
 
 export const VotePage = () => {
   return (
     <div>
       <Governance />
-      {/* <Distribution /> */}
-      {/* <Leaderboard /> */}
-      {/* <DetailsVotes /> */}
     </div>
   )
 }
