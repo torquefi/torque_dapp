9b242c809b7852a74b20ec4cc117751c769da49e
wormbsc <camconrad@gmail.com>
2023-12-24 09:23:19 -0800

    try fix build

--- constants/abi/index.ts
+++ constants/abi/index.ts
@@ -4,9 +4,9 @@ export { default as ethAbi } from './eth.json'
 // export { default as compAbi } from './comp.json'
 
 // export { default as boostBtcAbi } from './boostBtc.json'
-export { default as boostEth } from './boostEth.json'
+export { default as boostEthAbi } from './boostEth.json'
 // export { default as boostTorqAbi } from './boostTorq.json'
-// export { default as boostCompAbi } from './boostWbtc.json'
+// export { default as boostCompAbi } from './boostComp.json'
 
 export { default as compoundUsdc } from './compoundUsdc.json'
 
