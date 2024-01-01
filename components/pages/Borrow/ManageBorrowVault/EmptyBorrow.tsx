71544d7daac56e4cfc4db7cbe75053fcda499bd5
wormbsc <camconrad@gmail.com>
2023-12-31 21:47:45 -0800

    refine

--- components/pages/Borrow/ManageBorrowVault/EmptyBorrow.tsx
+++ components/pages/Borrow/ManageBorrowVault/EmptyBorrow.tsx
@@ -5,7 +5,7 @@ export const EmptyBorrow = () => {
   const theme = useSelector((store: AppStore) => store.theme.theme)
 
   return (
-    <div className="bg-white mt-[24px] overflow-hidden rounded-[12px] border border-neutral-200 dark:border-[#1a1a1a] dark:bg-[#030303]">
+    <div className="bg-transparent mt-[24px] overflow-hidden rounded-[12px] border border-neutral-200 dark:border-[#1a1a1a] dark:bg-[#030303]">
       <div
         className={
           'flex h-[300px] flex-col items-center justify-center bg-cover bg-top bg-no-repeat' +
@@ -28,7 +28,7 @@ export const EmptyBorrow = () => {
         <p className="font-larken text-[24px] text-[#404040] dark:text-white">
           No vaults created yet
         </p>
-        <p className="mt-[12px] max-w-[360px] text-center font-body text-[16px] text-[#959595]">
+        <p className="mt-[12px] mb-1 max-w-[360px] text-center font-body text-[16px] text-[#959595]">
           Deposit assets to create a Borrow vault & access a self-service line
           of credit. Repay as you prefer.
         </p>
