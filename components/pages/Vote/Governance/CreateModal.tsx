ea82e6d444485592ef43a054e8307ed25e714834
Anh Tran Duc <anh.td1401@gmail.com>
2024-01-25 11:45:16 +0700

    update skeleton & dropdown

--- components/pages/Vote/Governance/CreateModal.tsx
+++ components/pages/Vote/Governance/CreateModal.tsx
@@ -1,18 +1,58 @@
 import Modal from '@/components/common/Modal'
+import { AppStore } from '@/types/store'
+import { Menu, Transition } from '@headlessui/react'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { Fragment, useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import { AiOutlineClose } from 'react-icons/ai'
 import { FaAngleDown } from 'react-icons/fa'
 import { useSelector } from 'react-redux'
-import { AppStore } from '@/types/store'
+import * as yup from 'yup'
+
+const validationSchema = yup.object({
+  title: yup.string().trim(),
+  link: yup.string().trim(),
+  description: yup.string().trim(),
+  action: yup.string().trim(),
+})
+
+const defaultValues = {
+  title: '',
+  link: '',
+  description: '',
+  action: '',
+}
 
 export const CreateModal = (props: any) => {
   const { openModal, handleCLose } = props
   const theme = useSelector((store: AppStore) => store.theme.theme)
+
+  const form = useForm({
+    defaultValues: defaultValues,
+    resolver: yupResolver(validationSchema),
+  })
+  const values = form.watch()
+
+  const options = [
+    { value: 'TransferFromTreasury', label: 'Transfer from treasury' },
+    { value: 'SetPoolRewardSpeed', label: 'Set pool reward speed' },
+  ]
+  const optionLabel = options?.find(
+    (item) => item?.value === values?.action
+  )?.label
+
+  useEffect(() => {
+    if (openModal) {
+      form.reset()
+    }
+  }, [])
+
   return (
     <div>
       <Modal
         open={openModal}
         handleClose={handleCLose}
-        className="bg-[#FCFAFF] dark:bg-[#030303] mx-auto w-[90%] max-w-[380px] max-h-[420px] px-[24px] overflow-hidden hover:overflow-y-auto"
+        className="no-scrollbar mx-auto max-h-[420px] w-[90%] max-w-[380px] overflow-hidden bg-[#FCFAFF] px-[24px] hover:overflow-y-auto dark:bg-[#030303]"
         hideCloseIcon
       >
         <div className="flex items-center justify-between py-2">
@@ -24,32 +64,82 @@ export const CreateModal = (props: any) => {
             onClick={handleCLose}
           />
         </div>
-        <div className={`mt-2 hidden h-[1px] w-full md:block` +`
-      ${theme === 'light' ? 'bg-gradient-divider-light' : 'bg-gradient-divider'}`
-        }></div>
+        <div
+          className={
+            `mt-2 hidden h-[1px] w-full md:block` +
+            `
+      ${
+        theme === 'light' ? 'bg-gradient-divider-light' : 'bg-gradient-divider'
+      }`
+          }
+        ></div>
         <div className="mt-[22px]">
-          <input
-            className="w-full rounded-[12px] border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-transparent px-[21px] py-[16px] text-[16px] font-[500] text-[#959595]"
-            type="text"
-            placeholder="Title*"
-          />
-          <input
-            className="mt-[14px] w-full rounded-[12px] border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-transparent px-[21px] py-[16px] text-[16px] font-[500] text-[#959595]"
-            type="text"
-            placeholder="Forum link*"
-          />
-          <input
-            className="mt-[14px] w-full rounded-[12px] border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-transparent px-[21px] pb-[190px] pt-[20px] text-[16px] font-[500] text-[#959595]"
-            type="text"
-            placeholder="Description*"
-          />
-          <div className="mt-[14px] flex w-full items-center  justify-between rounded-[8px] border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] px-[21px] py-[12px]">
+          <div>
             <input
+              className="w-full rounded-[12px] border-[1px] border-solid border-[#E6E6E6] bg-transparent px-[21px] py-[16px] text-[16px] font-[500] text-[#959595] dark:border-[#1a1a1a]"
               type="text"
-              className="bg-transparent text-[16px] font-[500] text-[#959595]"
-              placeholder="Add action"
+              placeholder="Title*"
+              {...form.register('title')}
             />
-            <FaAngleDown className="h-[24px] cursor-pointer text-[#959595]" />
+          </div>
+          <div className="mt-[14px]">
+            <input
+              className="w-full rounded-[12px] border-[1px] border-solid border-[#E6E6E6] bg-transparent px-[21px] py-[16px] text-[16px] font-[500] text-[#959595] dark:border-[#1a1a1a]"
+              type="text"
+              placeholder="Forum link*"
+              {...form.register('link')}
+            />
+          </div>
+          <div className="mt-[14px]">
+            <input
+              className="w-full rounded-[12px] border-[1px] border-solid border-[#E6E6E6] bg-transparent px-[21px] pb-[190px] pt-[20px] text-[16px] font-[500] text-[#959595] dark:border-[#1a1a1a]"
+              type="text"
+              placeholder="Description*"
+              {...form.register('description')}
+            />
+          </div>
+          <div>
+            <Menu as="div" className="relative block text-left">
+              <div>
+                <Menu.Button className="mt-[14px] flex w-full items-center  justify-between rounded-[8px] border-[1px] border-solid border-[#E6E6E6] px-[21px] py-[12px] text-[#959595] dark:border-[#1a1a1a]">
+                  {optionLabel || 'Add action'}
+                  <FaAngleDown className="h-[24px] cursor-pointer text-[#959595]" />
+                </Menu.Button>
+              </div>
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="absolute bottom-[108%] right-0 mt-2 w-56 origin-bottom-right space-y-[2px] rounded-md bg-[#FCFAFF] py-[8px] shadow-sm dark:bg-[#191919]">
+                  {options.map((item, i) => (
+                    /* Use the `active` state to conditionally style the active item. */
+                    <Menu.Item key={i}>
+                      {({ active }) => (
+                        <button
+                          className={`${
+                            active || item?.value === values?.action
+                              ? 'bg-[#AA5BFF] text-white'
+                              : 'text-[#959595]'
+                          } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                          onClick={() =>
+                            form.setValue('action', item?.value, {
+                              shouldValidate: true,
+                            })
+                          }
+                        >
+                          {item?.label}
+                        </button>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </Menu.Items>
+              </Transition>
+            </Menu>
           </div>
         </div>
         <button
--- components/pages/Vote/Governance/VotingPower.tsx
+++ components/pages/Vote/Governance/VotingPower.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { DelegateModal } from './DelegateModal'
-import Link from 'next/link'
-import { useSelector } from 'react-redux'
-import { AppStore } from '@/types/store'
 import Popover from '@/components/common/Popover'
-import { torqContract, wethContract } from '@/constants/contracts'
-import Web3 from 'web3'
-import { useAccount } from 'wagmi'
-import { ethers } from 'ethers'
-import { NumericFormat } from 'react-number-format'
-import BigNumber from 'bignumber.js'
+import { torqContract } from '@/constants/contracts'
 import { useGetPriceTorqueToken } from '@/lib/hooks/usePriceToken'
+import { AppStore } from '@/types/store'
+import BigNumber from 'bignumber.js'
+import { ethers } from 'ethers'
+import Link from 'next/link'
+import { useEffect, useMemo, useState } from 'react'
+import { NumericFormat } from 'react-number-format'
+import { useSelector } from 'react-redux'
+import { useAccount } from 'wagmi'
+import Web3 from 'web3'
+import { DelegateModal } from './DelegateModal'
 
 export const VotingPower = () => {
   const [openDelegateModal, setOpenDelegateModal] = useState(false)
@@ -48,7 +48,7 @@ export const VotingPower = () => {
 
   return (
     <>
-      <div className="ml-0 ml-4 mr-2 mt-6 h-full w-full rounded-xl border bg-[#FFFFFF] from-[#0d0d0d] to-[#0d0d0d]/0 py-[12px] pb-[20px] pt-[16px] text-[#404040] md:ml-4 md:mr-0 md:mt-0 lg:max-w-[40%] xl:px-[32px] dark:border-[#1A1A1A] dark:bg-transparent dark:bg-gradient-to-br dark:text-white dark:text-white">
+      <div className="ml-0 mr-2 mt-6 h-full w-full rounded-xl border bg-[#FFFFFF] from-[#0d0d0d] to-[#0d0d0d]/0 px-[16px] py-[12px] pb-[20px] pt-[16px] text-[#404040] dark:border-[#1A1A1A] dark:bg-transparent dark:bg-gradient-to-br dark:text-white md:ml-4 md:mr-0 md:mt-0 lg:max-w-[40%] xl:px-[32px]">
         <div className="flex items-center justify-between">
           <h2 className="font-larken text-[24px] font-[400] leading-[40px]">
             Vote Power
@@ -68,11 +68,12 @@ export const VotingPower = () => {
           className={
             `mb-2 mt-2 hidden h-[1px] w-full md:block` +
             `
-      ${theme === 'light' ? 'bg-gradient-divider-light' : 'bg-gradient-divider'
-            }`
+      ${
+        theme === 'light' ? 'bg-gradient-divider-light' : 'bg-gradient-divider'
+      }`
           }
         ></div>
-        <div className="grid w-full h-auto grid-cols-2 gap-4 py-2 mb-2 overflow-y-auto">
+        <div className="mb-2 grid h-auto w-full grid-cols-2 gap-4 overflow-y-auto py-2">
           <div className="flex h-[102px] flex-col items-center justify-center  rounded-[8px] rounded-md  border border-[1px] border-[#1A1A1A] border-[#E6E6E6] bg-[#FCFCFC] from-[#161616] to-[#161616]/0 dark:border-[#1A1A1A] dark:bg-transparent dark:bg-gradient-to-b">
             <div className="font-larken mt-1 text-[24px] text-[#404040] dark:text-white">
               <NumericFormat
@@ -90,8 +91,8 @@ export const VotingPower = () => {
                 value={
                   address
                     ? new BigNumber(torqueBalance || 0)
-                      .multipliedBy(new BigNumber(torquePrice || 0))
-                      .toString()
+                        .multipliedBy(new BigNumber(torquePrice || 0))
+                        .toString()
                     : '0.00'
                 }
                 displayType="text"
@@ -106,7 +107,7 @@ export const VotingPower = () => {
         <div className="flex flex-col">
           <button
             onClick={() => setOpenDelegateModal(true)}
-            className={`font-mona w-full text-[14px] rounded-full border border-[#AA5BFF] bg-gradient-to-b from-[#AA5BFF] to-[#912BFF] py-1 uppercase text-white transition-all hover:border hover:border-[#AA5BFF] hover:from-transparent hover:to-transparent hover:text-[#AA5BFF]
+            className={`font-mona w-full rounded-full border border-[#AA5BFF] bg-gradient-to-b from-[#AA5BFF] to-[#912BFF] py-1 text-[14px] uppercase text-white transition-all hover:border hover:border-[#AA5BFF] hover:from-transparent hover:to-transparent hover:text-[#AA5BFF]
           `}
           >
             Delegate votes
@@ -114,7 +115,7 @@ export const VotingPower = () => {
           <Link
             href="https://bit.ly/torq-uniswap"
             target="_blank"
-            className="font-mona mt-2 w-full text-[14px] rounded-full border border-[#AA5BFF] bg-transparent py-1 text-center uppercase text-[#AA5BFF] transition-all"
+            className="font-mona mt-2 w-full rounded-full border border-[#AA5BFF] bg-transparent py-1 text-center text-[14px] uppercase text-[#AA5BFF] transition-all"
           >
             acquire torq
           </Link>
