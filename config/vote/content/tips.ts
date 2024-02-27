b53d78c4a13fe181081a272a304b00b3f4754ff6
wormbsc <camconrad@gmail.com>
2024-02-27 00:57:21 -0800

    refine

--- config/vote/content/tips.ts
+++ config/vote/content/tips.ts
@@ -3,21 +3,21 @@ import Tip2Md from '../description/tip-2.mdx'
 
 export const tipsData = [
   {
-    title: 'This is the TIP title 1',
-    stage: 'Active',
+    title: 'Incentivize Uniswap Liquidity',
+    stage: 'Pending',
     id: 1,
     description: Tip1Md,
-    timeVote: 'Vote hasn’t started',
+    timeVote: 'Vote has not started yet',
     voteRed: 0,
     voteGreen: 0,
   },
-  {
-    title: 'This is the TIP title 2',
-    stage: 'Pending',
-    id: 2,
-    description: Tip2Md,
-    timeVote: 'Vote hasn’t started',
-    voteRed: 0,
-    voteGreen: 0,
-  },
+  // {
+  //   title: 'This is the TIP title 2',
+  //   stage: 'Pending',
+  //   id: 2,
+  //   description: Tip2Md,
+  //   timeVote: 'Vote hasn’t started',
+  //   voteRed: 0,
+  //   voteGreen: 0,
+  // },
 ]
