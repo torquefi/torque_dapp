1b4c7fc986c02e5d6bb411838958d8dc233e9dbf
AnhTranDuc <ducanh.tran@finhay.com.vn>
2024-02-27 14:37:00 +0700

    update code

--- components/pages/Vote/Details/index.tsx
+++ components/pages/Vote/Details/index.tsx
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { InforVotes } from './Infor'
 import { MainContent } from './MainContent'
 import SkeletonDefault from '@/components/skeleton'
 import { useRouter } from 'next/router'
-import Markdown from 'react-markdown'
 import { useAppSelector } from '@/lib/redux/store'
 
 export const DetailsVotes = () => {
@@ -20,7 +19,7 @@ export const DetailsVotes = () => {
         setTipDetails(data)
       } else {
       }
-    } catch (error) {}
+    } catch (error) { }
     setTimeout(() => setIsLoading(false), 1000)
   }, [])
 
@@ -55,10 +54,11 @@ export const DetailsVotes = () => {
       </div>
     )
   }
+
   return (
     <div className="w-full mx-auto text-center">
       <h1 className="font-larken mx-auto mt-4 w-full text-[20px] font-[400] leading-[23px] text-[#030303] dark:text-white md:text-[36px] md:leading-[44px]">
-        TIP-{id}: {tipDetails.title}
+        TIP-{id}: {tipDetails?.title}
       </h1>
       <div className="mt-[14px]">
         <div className="mx-auto flex items-center justify-center gap-[8px]">
@@ -68,7 +68,7 @@ export const DetailsVotes = () => {
             </div>
           )}
           {tipDetails?.stage === 'Pending' && (
-            <div className="rounded-[6px] bg-[#FF9C414D] px-[12px] py-[2px] text-[12px] font-[500] uppercase text-[#FF9C41] shadow-md">
+            <div className="rounded-[6px] bg-[#FF9C414D] px-[12px] py-[2px] text-[12px] font-[500] uppercase text-[#FF9C41] shadow-md ">
               Pending
             </div>
           )}
