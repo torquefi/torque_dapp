b9c1de3c1bd26f9f8532d8559652c5b92e32dee9
wormbsc <camconrad@gmail.com>
2024-02-28 10:59:18 -0800

    refine

--- components/pages/Vote/LeaderBoard/index.tsx
+++ components/pages/Vote/LeaderBoard/index.tsx
@@ -55,14 +55,14 @@ export const LeaderBoard = () => {
 
   if (isLoading) {
     return (
-      <div className="pt-[20px] w-full max-w-[821px] m-auto">
+      <div className="pt-[20px] w-full max-w-[800px] m-auto">
         <SkeletonDefault className="h-[430px] w-full rounded-xl" />
       </div>
     )
   }
   return (
-    <div className="pt-[20px] w-full max-w-[821px] m-auto">
-      <div className="w-full rounded-[12px] border border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-[#ffffff] dark:bg-transparent dark:bg-gradient-to-br from-[#0d0d0d] to-[#0d0d0d]/0 text-[#030303] dark:text-white text-center px-[24px] py-[24px]">
+    <div className="pt-[20px] w-full max-w-[800px] m-auto">
+      <div className="w-full rounded-[12px] border border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-[#ffffff] dark:bg-transparent dark:bg-gradient-to-br from-[#0d0d0d] to-[#0d0d0d]/0 text-[#030303] dark:text-white text-center px-[24px] pt-[24px] pb-[12px]">
         <div className="flex items-center justify-between">
           <h2 className="font-larken text-[24px] font-[400]">
             Leaderboard
@@ -81,36 +81,19 @@ export const LeaderBoard = () => {
         <div className={`mt-4 hidden h-[1px] w-full md:block` + `
       ${theme === 'light' ? 'bg-gradient-divider-light' : 'bg-gradient-divider'}`
         }></div>
-        {/* <div className="mx-auto w-full py-[58px]">
-          <img
-            src={
-              theme === 'light'
-                ? '/assets/pages/vote/genover/noproposal-white.png'
-                : '/assets/pages/vote/genover/noproposal.png'
-            }
-            alt=""
-            className="mx-auto w-full max-w-[84px]"
-          />
-          <h3 className="font-larken mt-[12px] text-center text-[24px] font-[400] leading-[34px]">
-            Delegates not listed yet
-          </h3>
-          <p className="mx-auto mt-[6px] w-full text-center text-[16px] font-[500] max-w-[320px] text-[#959595]">
-            The official release of Torque is coming soon. Follow our socials for updates.
-          </p>
-        </div> */}
-        <table className="w-full max-w-[821px]">
+        <table className="w-full max-w-[800px]">
           <thead>
             <tr>
-              <th className="whitespace-nowrap py-[16px] text-left text-[12px] font-[500] text-[#959595] md:w-[25%] md:text-[16px]">
+              <th className="whitespace-nowrap py-[12px] text-left text-[12px] font-[500] text-[#959595] md:w-[25%] md:text-[16px]">
                 Position
               </th>
-              <th className="whitespace-nowrap py-[16px] text-left text-[12px] font-[500] text-[#959595] md:w-[30%] md:text-[16px]">
+              <th className="whitespace-nowrap py-[12px] text-left text-[12px] font-[500] text-[#959595] md:w-[30%] md:text-[16px]">
                 Holder
               </th>
-              <th className="whitespace-nowrap py-[16px] text-left text-[12px] font-[500] text-[#959595] md:w-[30%] md:text-[16px]">
+              <th className="whitespace-nowrap py-[12px] text-left text-[12px] font-[500] text-[#959595] md:w-[30%] md:text-[16px]">
                 Size
               </th>
-              <th className="whitespace-nowrap py-[16px] text-left text-[12px] font-[500] text-[#959595] md:w-[25%] md:text-[16px]">
+              <th className="whitespace-nowrap py-[12px] text-left text-[12px] font-[500] text-[#959595] md:w-[25%] md:text-[16px]">
                 Value
               </th>
             </tr>
@@ -118,10 +101,10 @@ export const LeaderBoard = () => {
           {(topHolders || [])?.slice(3, 13).map((item, i) => (
             <tbody key={i}>
               <tr className="relative">
-                <td className="py-[16px] text-left text-[12px] md:text-[16px]">
+                <td className="py-[12px] text-left text-[12px] md:text-[16px]">
                   #{i + 1}
                 </td>
-                <td className="py-[16px] text-left text-[12px] md:text-[16px]">
+                <td className="py-[12px] text-left text-[12px] md:text-[16px]">
                   <div className="flex items-center justify-start gap-[16px]">
                     {/* <img
                       src={item.imgMain}
@@ -131,7 +114,7 @@ export const LeaderBoard = () => {
                     <p>{shortenAddress(item?.walletAddress)}</p>
                   </div>
                 </td>
-                <td className="py-[16px] text-left text-[12px] md:text-[16px]">
+                <td className="py-[12px] text-left text-[12px] md:text-[16px]">
                   {/* <NumericFormat
                     displayType='text'
                     value={item?.amount || 0}
@@ -140,7 +123,7 @@ export const LeaderBoard = () => {
                   /> */}
                   {toMetricUnits(item?.amount || 0)}
                 </td>
-                <td className="py-[16px] text-left text-[12px] md:text-[16px]">
+                <td className="py-[12px] text-left text-[12px] md:text-[16px]">
                   {/* <NumericFormat
                     displayType='text'
                     value={item?.amountUsd || 0}
@@ -173,7 +156,7 @@ export const LeaderBoard = () => {
         ></div>
         <Link href='https://arbiscan.io/token/0xb56c29413af8778977093b9b4947efeea7136c36#balances' legacyBehavior>
           <a target='_blank'>
-            <div className="mt-[18px] cursor-pointer text-center text-[14px] font-[500] uppercase text-[#959595]">
+            <div className="mt-[12px] cursor-pointer text-center text-[14px] font-[500] uppercase text-[#959595]">
               view all
             </div>
           </a>
