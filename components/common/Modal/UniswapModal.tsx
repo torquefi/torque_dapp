fda7f373723dbb0082413be5c8b9e5f2d4dc06f2
AnhTranDuc <ducanh.tran@finhay.com.vn>
2024-03-13 10:12:19 +0700

    update uniswap

--- components/common/Modal/UniswapModal.tsx
+++ components/common/Modal/UniswapModal.tsx
@@ -13,15 +13,16 @@ export default function UniSwapModal({ open, handleClose }: UniSwapModalProps) {
 
     return (
         <Modal
-            className="mx-auto w-[90%] max-w-[440px] bg-[#FCFAFF] dark:bg-[#030303] px-[1px] py-[16px]"
+            className="mx-auto w-[90%] max-w-[440px] bg-[#FCFAFF] dark:bg-[#030303] px-[0px] py-[1px]"
             open={open}
             handleClose={handleClose}
             hideCloseIcon
         >
             <iframe
                 src={`https://app.uniswap.org/#/swap?inputCurrency=0x82aF49447D8a07e3bd95BD0d56f35241523fBab1&outputCurrency=0xb56c29413af8778977093b9b4947efeea7136c36&theme=${theme === 'light' ? 'light' : 'dark'}`}
-                height="360px"
+                height="420px"
                 width="100%"
+                style={{ borderRadius: '24px', overflow: 'auto' }}
             />
         </Modal>
     )
