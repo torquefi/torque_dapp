954414a817f89dcc5f75508b6ebc46e943c75aef
AnhTranDuc <ducanh.tran@finhay.com.vn>
2024-04-26 10:57:09 +0700

    fix bugs 04 26

--- components/pages/Vote/Governance/OptionToken.tsx
+++ components/pages/Vote/Governance/OptionToken.tsx
@@ -24,12 +24,12 @@ export const OptionToken = () => {
 
   return (
     <div className="mt-[42px] items-center justify-between md:flex">
-      <div className="mt-[20px] flex w-full items-center justify-between rounded-[12px] border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-white dark:bg-transparent dark:from-[#0d0d0d] dark:to-[#0d0d0d]/0 px-[32px] py-[20px] dark:bg-gradient-to-br md:mt-0 md:w-[32.5%]">
+      <div className="mt-[20px] flex w-full items-center justify-between rounded-[12px] border-[1px] border-solid border-[#E6E6E6] dark:border-[#1a1a1a] bg-white dark:bg-transparent dark:from-[#0d0d0d] dark:to-[#0d0d0d]/0 px-[12px] xl:px-[32px] py-[10px] xl:py-[20px] dark:bg-gradient-to-br md:mt-0 md:w-[32.5%]">
         <div className="w-[50%]">
-          <h4 className="font-larken text-[#030303] dark:text-white text-[24px] font-[400] leading-[40px]">
+          <h4 className="font-larken text-[#030303] dark:text-white text-[16px] lg:text-[20px] xl:text-[24px] font-[400] leading-[40px]">
             16,000,000,000
           </h4>
-          <p className="font-[500] leading-[24px] text-[#959595]">
+          <p className="font-[500] text-[14px] lg:text-[16px] leading-[24px] text-[#959595] whitespace-nowrap">
             TORQ Remaining
           </p>
         </div>
@@ -38,7 +38,7 @@ export const OptionToken = () => {
             href="/vote/distribution"
             className="mb-[20px] flex cursor-pointer items-center justify-end gap-[5px]"
           >
-            <p className="text-[14px] font-[500] uppercase  text-[#AA5BFF]">
+            <p className="text-[14px] xl:text-[14px] font-[500] uppercase  text-[#AA5BFF]">
               learn
             </p>
             <img src="/assets/pages/vote/genover/next.svg" alt="" className="mb-1" />
@@ -48,12 +48,12 @@ export const OptionToken = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[20px] flex w-full items-center justify-between rounded-[12px] border-[1px] border-solid  border-[#E6E6E6] dark:border-[#1a1a1a]  bg-white dark:bg-transparent dark:from-[#0d0d0d] dark:to-[#0d0d0d]/0 px-[32px] py-[20px] dark:bg-gradient-to-br md:mt-0 md:w-[32.5%]">
+      <div className="mt-[20px] flex w-full items-center justify-between rounded-[12px] border-[1px] border-solid  border-[#E6E6E6] dark:border-[#1a1a1a]  bg-white dark:bg-transparent dark:from-[#0d0d0d] dark:to-[#0d0d0d]/0 px-[12px] xl:px-[32px] py-[10px] xl:py-[20px] dark:bg-gradient-to-br md:mt-0 md:w-[32.5%]">
         <div className="w-[50%]">
-          <h4 className="font-larken text-[#030303] dark:text-white text-[24px] font-[400] leading-[40px]">
+          <h4 className="font-larken text-[#030303] dark:text-white text-[16px] lg:text-[20px] xl:text-[24px] font-[400] leading-[40px]">
             11,121,512,940
           </h4>
-          <p className="font-[500] leading-[24px] text-[#959595]">
+          <p className="font-[500] text-[14px] lg:text-[16px] leading-[24px] text-[#959595] whitespace-nowrap">
             Circulating Supply
           </p>
         </div>
@@ -65,7 +65,7 @@ export const OptionToken = () => {
             rel="noopener noreferrer"
 
           >
-            <p className="text-[14px] font-[500] uppercase  leading-[40px] text-[#AA5BFF]">
+            <p className="text-[14px] xl:text-[14px] font-[500] uppercase  leading-[40px] text-[#AA5BFF]">
               Explorer
             </p>
             <img src="/assets/pages/vote/genover/next.svg" alt="" className="mb-1" />
@@ -76,16 +76,16 @@ export const OptionToken = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[20px] flex w-full items-center justify-between rounded-[12px] border-[1px] border-solid  border-[#E6E6E6] dark:border-[#1a1a1a]  bg-white dark:bg-transparent dark:from-[#0d0d0d] dark:to-[#0d0d0d]/0 px-[32px] py-[20px] dark:bg-gradient-to-br md:mt-0 md:w-[32.5%]">
+      <div className="mt-[20px] flex w-full items-center justify-between rounded-[12px] border-[1px] border-solid  border-[#E6E6E6] dark:border-[#1a1a1a]  bg-white dark:bg-transparent dark:from-[#0d0d0d] dark:to-[#0d0d0d]/0 px-[12px] xl:px-[32px] py-[10px] xl:py-[20px] dark:bg-gradient-to-br md:mt-0 md:w-[32.5%]">
         <div className="w-[50%]">
-          <h4 className="font-larken text-[#030303] dark:text-white text-[24px] font-[400] leading-[40px]">
+          <h4 className="font-larken text-[#030303] dark:text-white text-[16px] lg:text-[20px] xl:text-[24px]] font-[400] leading-[40px]">
             <NumericFormat
               displayType='text'
               value={holderAddress}
               thousandSeparator
             />
           </h4>
-          <p className="font-[500] leading-[24px] text-[#959595]">
+          <p className="font-[500] text-[14px] lg:text-[16px] leading-[24px] text-[#959595]">
             Global Holders
           </p>
         </div>
@@ -94,7 +94,7 @@ export const OptionToken = () => {
             href="/vote/leaderboard"
             className="flex cursor-pointer items-center justify-end gap-[5px]"
           >
-            <p className="text-[14px] font-[500] uppercase  leading-[40px] text-[#AA5BFF]">
+            <p className="text-[14px] xl:text-[14px] font-[500] uppercase  leading-[40px] text-[#AA5BFF]">
               view
             </p>
             <img src="/assets/pages/vote/genover/next.svg" alt="" className="mb-1" />
