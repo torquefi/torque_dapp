752126d3ddc05696c93265c8c3102ffd4cbf2132
AnhTranDuc <ducanh.tran@finhay.com.vn>
2024-05-08 18:06:09 +0700

    add new borrow simple

--- components/pages/Borrow/CreateBorrowVault/createBorrowItem.tsx
+++ components/pages/Borrow/CreateBorrowVault/createBorrowItem.tsx
@@ -27,7 +27,6 @@ export default function CreateBorrowItem({
 }: CreateBorrowItemProps) {
   console.log('item :>> ', item)
   const web3 = new Web3(Web3.givenProvider)
-  const { open } = useWeb3Modal()
 
   const [dataBorrow, setDataBorrow] = useState(item)
   const [isLoading, setIsLoading] = useState(true)
@@ -43,6 +42,8 @@ export default function CreateBorrowItem({
   const [isUsdBorrowToken, setIsUsdBorrowToken] = useState(true)
   const [isUsdDepositToken, setIsUsdDepositToken] = useState(true)
   const [isOpenConnectWalletModal, setOpenConnectWalletModal] = useState(false)
+  const isUsdcBorrowed = item.borrowTokenSymbol === 'USDC'
+  console.log('isUsdcBorrowed :>> ', isUsdcBorrowed);
 
   useEffect(() => {
     setTimeout(() => setIsLoading(false), 1000)
@@ -120,10 +121,6 @@ export default function CreateBorrowItem({
       toast.error(`You must supply ${item.depositTokenSymbol} to borrow`)
       return
     }
-    // if (amountReceive <= 0) {
-    //   toast.error('Can not borrow less than 0 TUSD')
-    //   return
-    // }
     try {
       setIsLoading(true)
       if (item.depositTokenSymbol == 'WBTC') {
@@ -197,19 +194,9 @@ export default function CreateBorrowItem({
             item?.borrowContractInfo?.address,
             '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
           )
-          // .send({
-          //   from: address,
-          // })
           await tx.wait()
         }
-
-        // await borrowContract.methods
-        //   .borrow(borrow.toString(), newUsdcBorrowAmount, tusdBorrowAmount)
-        //   .send({
-        //     from: address,
-        //     gasPrice: '5000000000'
-        //   })
-
+      
         const borrowContract2 = new ethers.Contract(
           item?.borrowContractInfo?.address,
           item?.borrowContractInfo?.abi,
@@ -354,7 +341,7 @@ export default function CreateBorrowItem({
               src={dataBorrow.depositTokenIcon}
               alt=""
             />
-            <div className="ml-[-4px] mt-[-4px] font-rogan text-[20px] leading-tight text-[#030303] dark:text-white md:text-[22px] lg:text-[26px]">
+            <div className="font-rogan ml-[-4px] mt-[-4px] text-[20px] leading-tight text-[#030303] dark:text-white md:text-[22px] lg:text-[26px]">
               Supply {dataBorrow.depositTokenSymbol},<br /> Borrow{' '}
               {dataBorrow.borrowTokenSymbol}
             </div>
@@ -379,7 +366,7 @@ export default function CreateBorrowItem({
             </Link>
           </Popover>
         </div>
-        <div className="grid grid-cols-2 gap-4 mt-1 mb-1 font-rogan">
+        <div className="font-rogan mb-1 mt-1 grid grid-cols-2 gap-4">
           <div className="flex w-full items-center justify-center rounded-md border bg-[#FCFCFC] from-[#161616] to-[#161616]/0  dark:border-[#1A1A1A] dark:bg-transparent dark:bg-gradient-to-b lg:h-[140px]">
             <InputCurrencySwitch
               tokenSymbol={item?.depositTokenSymbol}
@@ -427,7 +414,7 @@ export default function CreateBorrowItem({
         </div>
         <div className="flex items-center justify-between py-4 text-[#959595]">
           <div className="flex items-center justify-center">
-            <div>Loan providers</div>
+            <div>{item.multiLoan ? 'Loan providers' : 'Loan provider'}</div>
             <Popover
               trigger="hover"
               placement="top-left"
@@ -455,17 +442,19 @@ export default function CreateBorrowItem({
                 className="w-[24px]"
               />
             </Link>
-            {/* <Link
-              href={'https://tusd.torque.fi/'}
-              className=""
-              target={'_blank'}
-            > */}
-            <img
-              src={item.borrowTokenIcon}
-              alt={item.borrowTokenSymbol}
-              className="w-[24px]"
-            />
-            {/* </Link> */}
+            {item.borrowTokenIcon && (
+              <Link
+                href={'https://tusd.torque.fi/'}
+                className=""
+                target={'_blank'}
+              >
+                <img
+                  src={item.borrowTokenIcon}
+                  alt={item.borrowTokenSymbol}
+                  className="w-[24px]"
+                />
+              </Link>
+            )}
           </div>
         </div>
         <div className="flex justify-between text-[#959595]">
@@ -538,8 +527,9 @@ export default function CreateBorrowItem({
           </p>
         </div>
         <button
-          className={`font-rogan-regular mt-4 w-full rounded-full border border-[#AA5BFF] bg-gradient-to-b from-[#AA5BFF] to-[#912BFF] py-1 text-[14px] uppercase text-white transition-all hover:border hover:border-[#AA5BFF] hover:from-transparent hover:to-transparent hover:text-[#AA5BFF] ${buttonLoading && 'cursor-not-allowed opacity-50'
-            }`}
+          className={`font-rogan-regular mt-4 w-full rounded-full border border-[#AA5BFF] bg-gradient-to-b from-[#AA5BFF] to-[#912BFF] py-1 text-[14px] uppercase text-white transition-all hover:border hover:border-[#AA5BFF] hover:from-transparent hover:to-transparent hover:text-[#AA5BFF] ${
+            buttonLoading && 'cursor-not-allowed opacity-50'
+          }`}
           disabled={buttonLoading != ''}
           onClick={() => {
             if (
