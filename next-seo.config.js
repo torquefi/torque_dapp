fc5ac8749d55ee19698bdb63a3df64ebc32aebf1
camconrad <camconrad@gmail.com>
2024-11-02 07:17:26 -0700

    refine

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,29 +1,55 @@
 export default {
   title: 'Torque',
   titleTemplate: '%s',
-  // canonical: 'https://torque.fi',
+  canonical: 'https://app.torque.fi',
   description:
-    'Torque is a crypto wealth management platform that aggregates yield and loans from leading blockchain protocols while enabling one-click borrowing through non-custodial PBLOCs (portfolio-backed lines of credit). Torque utilizes smart contract automation to enable self-driving money. Our Vaults-as-a-Service model helps investors, institutions, and DAOs earn passive income and access crypto loans while retaining full custody.',
+    'Torque is an automation protocol to streamline smart contract interactions. It unifies the finest non-custodial opportunities on the blockchain to satisfy the key components of wealth generation while reducing friction, increasing transparency, and guaranteeing self-custody.',
   additionalMetaTags: [
     {
       name: 'keywords',
       content:
-        'Torque, crypto wealth management platform, yield, loans, blockchain protocols, borrow, PBLOCs (portfolio-backed lines of credit), smart contract automation, self-driving money, Vaults-as-a-Service, investors, institutions, DAOs, passive income, crypto loans, custody',
+        'Torque, crypto wealth managemen, yield, loans, blockchain, borrow, PBLOCs, portfolio-backed lines of credit, smart contract automation, self-driving money, Vaults-as-a-Service, investors, institutions, DAOs, passive income, crypto loans, self custody',
     },
+    { name: 'robots', content: 'index, follow' },
+    { name: 'author', content: 'Torque Team' },
   ],
   openGraph: {
     type: 'website',
     locale: 'en_US',
-    url: 'https://torque.fi',
+    url: 'https://app.torque.fi',
     site_name: 'Torque',
+    description:
+      'Torque is an automation protocol to streamline smart contract interactions. It unifies the finest non-custodial opportunities on the blockchain to satisfy the key components of wealth generation while reducing friction, increasing transparency, and guaranteeing self-custody.',
     images: [
       {
-        url: 'https://uploads-ssl.webflow.com/6556f6be06fc2abb8a8da998/663729d31d558fc6efc405ae_torque-finance.png',
+        url: 'https://uploads-ssl.webflow.com/6556f6be06fc2abb8a8da998/668e0c1fe2e01cba33b85485_torque.png',
         width: 1200,
         height: 630,
         alt: 'Torque',
         type: 'image/*',
       },
+    ],
+  },
+  twitter: {
+    handle: '@torquefi',
+    site: '@torquefi',
+    cardType: 'summary_large_image',
+    image: 'https://uploads-ssl.webflow.com/6556f6be06fc2abb8a8da998/668e0c1fe2e01cba33b85485_torque.png',
+    title: 'Torque',
+    description:
+      'Torque is an automation protocol to streamline smart contract interactions. It unifies the finest non-custodial opportunities on the blockchain to satisfy the key components of wealth generation while reducing friction, increasing transparency, and guaranteeing self-custody.',
+  },
+  schema: {
+    '@context': 'https://schema.org',
+    '@type': 'Organization',
+    name: 'Torque',
+    url: 'https://app.torque.fi',
+    logo: 'https://cdn.prod.website-files.com/6556f6be06fc2abb8a8da998/6556f84d83ff0fa1fa70541a_torque-logo.png',
+    sameAs: [
+      'https://twitter.com/torquefi',
+      'https://linkedin.com/company/torquefi',
+      'https://youtube.com/@torquefi',
+      'https://github.com/torquefi',
     ],
   },
 }
