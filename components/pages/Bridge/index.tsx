4f720263f3740a978cf427e8e90e90411d6bd685
camconrad <camconrad@gmail.com>
2024-12-06 21:27:30 -0800

    refine

--- components/pages/Bridge/index.tsx
+++ components/pages/Bridge/index.tsx
@@ -3,7 +3,7 @@ import BridgeAssets from './BridgeAssets'
 
 export const BridgePage = () => {
   return (
-    <div className="mt-[32px] md:mt-[20px]">
+    <div className="">
       <BridgeAssets/>
     </div>
   )
